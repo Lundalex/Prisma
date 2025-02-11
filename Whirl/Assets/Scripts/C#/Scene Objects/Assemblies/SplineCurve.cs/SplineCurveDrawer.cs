ad07ba99008edd11b01471c188e4aa9512db6b92
abbnoamar <Noah.Marklund@abbgymnasiet.se>
2025-02-11 16:28:39 +0100

    Smart spline drawer functionality

--- Whirl/Assets/Scripts/C#/Scene Objects/Assemblies/SplineCurve.cs/SplineCurveDrawer.cs
+++ Whirl/Assets/Scripts/C#/Scene Objects/Assemblies/SplineCurve.cs/SplineCurveDrawer.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 using Resources2;
 
-[ExecuteInEditMode]
-public class SplineCurveDrawer : EditorLifeCycle
+[ExecuteAlways]
+public class SplineCurveDrawer : MonoBehaviour
 {
     [Header("Curve Settings")]
     [SerializeField] private RectTransform[] handleRects;
@@ -12,11 +15,53 @@ public class SplineCurveDrawer : EditorLifeCycle
     [SerializeField] private Color editorCanvasCurveColor = Color.green;
     [SerializeField] private Color editorSimCurveColor = Color.blue;
     [SerializeField] private float boxBottom = 0f;
+    [SerializeField] private float boxLeft = 0f;
+    [SerializeField] private float boxRight = 0f;
+
+    [Header("Unity Event")]
+    public UnityEvent onValueChangeDone;
+
+    [Header("References")]
+    [SerializeField] private DataStorage dataStorage;
+    [SerializeField] private DataStorage dataStorageB;
 
     [SerializeField, HideInInspector] private Vector3[] lastHandlePositions;
 
     // Private references
     private Main main;
+
+    // Other
+    private bool storedDataInitialized = false;
+    private bool isMonitoringValueChange = false;
+    private float[] previousSliderValues;
+
+    private void CheckStoredData()
+    {
+        if (Application.isPlaying && !storedDataInitialized)
+        {
+            float[] storedSliderValues = dataStorage.GetValue<float[]>();
+            if (storedSliderValues != null && handleRects != null && storedSliderValues.Length == handleRects.Length)
+            {
+                for (int i = 0; i < handleRects.Length; i++)
+                {
+                    if (handleRects[i] != null)
+                    {
+                        Slider slider = handleRects[i].GetComponentInParent<Slider>();
+                        if (slider != null)
+                        {
+                            slider.value = storedSliderValues[i];
+                        }
+                    }
+                }
+            }
+            Vector3[] storedPositions = dataStorageB.GetValue<Vector3[]>();
+            if (storedPositions != null && handleRects != null && storedPositions.Length == handleRects.Length)
+            {
+                lastHandlePositions = storedPositions;
+            }
+            storedDataInitialized = true;
+        }
+    }
 
     private void OnDrawGizmos()
     {
@@ -26,19 +71,56 @@ public class SplineCurveDrawer : EditorLifeCycle
         DrawBoxSplineInEditor(editorSimCurveColor, true); // Sim space
     }
 
-    public override void OnEditorUpdate()
+    public void Update()
     {
-        if (!Application.isPlaying)
+        if (handleRects != null && handleRects.Length > 0)
         {
-            if (handleRects != null && handleRects.Length > 0)
+            float[] sliderValues = new float[handleRects.Length];
+            for (int i = 0; i < handleRects.Length; i++)
             {
-                lastHandlePositions = new Vector3[handleRects.Length];
-                for (int i = 0; i < handleRects.Length; i++)
+                if (handleRects[i] != null)
                 {
-                    if (handleRects[i] != null)
-                        lastHandlePositions[i] = handleRects[i].position;
+                    Slider slider = handleRects[i].GetComponentInParent<Slider>();
+                    if (slider != null)
+                    {
+                        sliderValues[i] = slider.value;
+                    }
                 }
             }
+            
+            if (previousSliderValues == null)
+            {
+                previousSliderValues = sliderValues;
+            }
+            else
+            {
+                bool valueChanged = false;
+                for (int i = 0; i < sliderValues.Length; i++)
+                {
+                    if (sliderValues[i] != previousSliderValues[i])
+                    {
+                        valueChanged = true;
+                        break;
+                    }
+                }
+                if (valueChanged && !isMonitoringValueChange)
+                {
+                    StartMonitoringValueChange();
+                }
+                previousSliderValues = sliderValues;
+            }
+            
+            if(dataStorage != null)
+                dataStorage.SetValue(sliderValues);
+
+            lastHandlePositions = new Vector3[handleRects.Length];
+            for (int i = 0; i < handleRects.Length; i++)
+            {
+                if (handleRects[i] != null)
+                    lastHandlePositions[i] = handleRects[i].position;
+            }
+            if(dataStorageB != null)
+                dataStorageB.SetValue(lastHandlePositions);
         }
     }
 
@@ -57,7 +139,9 @@ public class SplineCurveDrawer : EditorLifeCycle
  
     public Vector3[] CreateSplinePoints(bool simSpace_canvasSpace)
     {
-        List<Vector3> points = new List<Vector3>();
+        CheckStoredData();
+
+        List<Vector3> points = new();
 
         Vector3[] positions = new Vector3[handleRects.Length];
         for (int i = 0; i < handleRects.Length; i++)
@@ -93,30 +177,65 @@ public class SplineCurveDrawer : EditorLifeCycle
 
     public Vector3[] CreateBoxSplinePoints(bool simSpace_canvasSpace, bool closeLoop)
     {
+        CheckStoredData();
+
         Vector3[] topCurve = CreateSplinePoints(simSpace_canvasSpace);
         if (topCurve.Length < 2)
             return topCurve;
         
-        List<Vector3> points = new List<Vector3>();
+        List<Vector3> points = new();
+        
+        // Add the top curve points
         points.AddRange(topCurve);
-        float bottomY = GetBoxBottom(simSpace_canvasSpace);
-        Vector3 lastTop = topCurve[^1];
-        points.Add(new Vector3(lastTop.x, bottomY, lastTop.z));
+
+        // Compute extrusion values using shared logic
+        float extrudedBottomY = GetExtrusionValue(boxBottom, simSpace_canvasSpace, false);
+        float leftOffset = GetExtrusionValue(boxLeft, simSpace_canvasSpace, true);
+        float rightOffset = GetExtrusionValue(boxRight, simSpace_canvasSpace, true);
+
         Vector3 firstTop = topCurve[0];
-        points.Add(new Vector3(firstTop.x, bottomY, firstTop.z));
+        Vector3 lastTop = topCurve[topCurve.Length - 1];
+
+        // Create extruded vertices for straight, perpendicular side edges
+        Vector3 leftTop = new Vector3(firstTop.x + leftOffset, firstTop.y, firstTop.z);
+        Vector3 rightTop = new Vector3(lastTop.x + rightOffset, lastTop.y, lastTop.z);
+        Vector3 leftBottom = new Vector3(firstTop.x + leftOffset, extrudedBottomY, firstTop.z);
+        Vector3 rightBottom = new Vector3(lastTop.x + rightOffset, extrudedBottomY, lastTop.z);
+
+        // Construct the polygon:
+        // top curve -> right edge (top then bottom) -> bottom edge -> left edge (bottom then top) -> close
+        points.Add(rightTop);
+        points.Add(rightBottom);
+        points.Add(leftBottom);
+        points.Add(leftTop);
         if (closeLoop) points.Add(firstTop);
         
         return points.ToArray();
     }
 
-    private float GetBoxBottom(bool simSpace_canvasSpace)
+    private float GetExtrusionValue(float value, bool simSpace, bool isHorizontal)
     {
-        if (simSpace_canvasSpace)
-            return boxBottom;
+        if (simSpace)
+            return value;
         if (main == null) main = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Main>();
         Vector2 res = Func.Int2ToVector2(main.Resolution);
         Vector2 boundary = Func.Int2ToVector2(main.BoundaryDims);
-        return boxBottom / boundary.y * res.y;
+        return isHorizontal ? value / boundary.x * res.x : value / boundary.y * res.y;
+    }
+
+    private float GetBoxBottom(bool simSpace_canvasSpace)
+    {
+        return GetExtrusionValue(boxBottom, simSpace_canvasSpace, false);
+    }
+
+    private float GetBoxLeft(bool simSpace_canvasSpace)
+    {
+        return GetExtrusionValue(boxLeft, simSpace_canvasSpace, true);
+    }
+
+    private float GetBoxRight(bool simSpace_canvasSpace)
+    {
+        return GetExtrusionValue(boxRight, simSpace_canvasSpace, true);
     }
 
     private Vector3 TransformPoint(Vector3 point, bool simSpace_canvasSpace)
@@ -131,5 +250,22 @@ public class SplineCurveDrawer : EditorLifeCycle
     {
         if (main == null) main = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Main>();
         coords = (Vector2)coords / Func.Int2ToVector2(main.Resolution) * Func.Int2ToVector2(main.BoundaryDims);
+    }
+    
+    public void StartMonitoringValueChange()
+    {
+        if (!isMonitoringValueChange)
+            StartCoroutine(WaitForMouseRelease());
+    }
+    
+    private IEnumerator WaitForMouseRelease()
+    {
+        isMonitoringValueChange = true;
+        while (Input.GetMouseButton(0))
+        {
+            yield return null;
+        }
+        onValueChangeDone.Invoke();
+        isMonitoringValueChange = false;
     }
 }
